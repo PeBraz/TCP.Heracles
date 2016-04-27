196569e69d1e284c2beafdffa6453e5b646ffd49
pebraz <pbraz.93@gmail.com>
2016-04-27 13:53:56 +0100

    refactoring done to logging script, correctly creates separate files from a log file

--- log/log_reader.py
+++ log/log_reader.py
@@ -25,24 +25,24 @@ gnuplot_base = ("set terminal jpeg size 1280,720\n"
 	"plot {plots}\n"
 	"exit")
 
-
-gnuplot_cwnd = ("set terminal jpeg size 1280,720\n"
-	"set output \"{fname}_cwnd.jpeg\"\n"
-	"set size 1,1\n"
-	"set grid y\n"
-	"plot \"{fname}.csv\" using 1:5 with linespoints title \"{fname}\"\n"
-	"exit")
-
-
+#
+#gnuplot_cwnd = ("set terminal jpeg size 1280,720\n"
+#	"set output \"{fname}_cwnd.jpeg\"\n"
+#	"set size 1,1\n"
+#	"set grid y\n"
+#	"plot \"{fname}.csv\" using 1:5 with linespoints title \"{fname}\"\n"
+#	"exit")
+#
+#
 #create_script("string_rtt", [rtt, srtt, mdev],[string.csv],[RTT, SRTT, MDEV])
-gnuplot_rtt = ("set terminal jpeg size 1280,720\n"
-	"set output \"{fname}_rtt.jpeg\"\n"
-	"set size 1,1\n"
-	"set grid y\n"
-	"plot \"{fname}.csv\" using 1:7 with linespoints title \"rtt\","
-	" \"{fname}.csv\" using 1:8 with linespoints title \"srtt\","
-	" \"{fname}.csv\" using 1:9 with linespoints title \"mdev\"\n"
-	"exit")
+#gnuplot_rtt = ("set terminal jpeg size 1280,720\n"
+#	"set output \"{fname}_rtt.jpeg\"\n"
+#	"set size 1,1\n"
+#	"set grid y\n"
+#	"plot \"{fname}.csv\" using 1:7 with linespoints title \"rtt\","
+#	" \"{fname}.csv\" using 1:8 with linespoints title \"srtt\","
+#	" \"{fname}.csv\" using 1:9 with linespoints title \"mdev\"\n"
+#	"exit")
 
 
 def create_script(name, name_list, csv_list, attr_list):
@@ -51,10 +51,13 @@ def create_script(name, name_list, csv_list, attr_list):
 	return gnuplot_base.format(fname=name, plots=plots)
 
 #
-# @intercal boolean		if true then numbering will intercalate index values
+# @join boolean		if true then numbering will intercalate index values
 #						between files, else numbering is independent
 #
-#	Intercalated:                   Non-Intercalated:
+#
+#	join is used when the order of events is important.
+#
+#	joined:                   Non-joined:
 #	FILE1		FILE2				FILE1		FILE2	
 #	|1	|		|2	| 				|1	|		|1	| 	
 #	|3	|		|5	|		VS. 	|2	|		|2	|
@@ -73,25 +76,63 @@ def log_to_csv(log_data, intercal=False):
 	return d
 
 
-if __name__ == '__main__':
 
-	INTERCAL = True
+help_str = ("usage: ./log_reader.py <log file>\n"
+			"options:\n"
+				"\t--help - print this information\n"
+				"\t--join - concatenate information from differente connections\n")
 
-	logname = sys.argv[1]
+#
+#	Creates a csv file from the dmesg output
+#	@logname - logging filename
+#	@join - optional flag indicating if numbering should be intesected or independent
+#
+#	@returns  name root of the generated .csv files
+#
+
+def log_reader(logname, join=False):
 	fname = logname.split('.')[0]
 	files = []
+	
 	with open(logname) as log:
-		conn_log = log_to_csv(log.read(), INTERCAL)
-		for i, logs in conn_log.iteritems():
-			files.append("{}_{}".format(fname, i))
-			with open("{}_{}.csv".format(fname, i), 'w+') as csv:
-				csv.write('\n'.join(logs))
+		conn_log = log_to_csv(log.read(), join)
+		
+	for i, logs in conn_log.iteritems():
+		files.append("{}_{}".format(fname, i))
+		with open("{}_{}.csv".format(fname, i), 'w+') as csv:
+			csv.write('\n'.join(logs))
+	return files
 
-	if INTERCAL:
-		sc2 = create_script(fname + "_rtt", ["rtt","srtt","mdev"]*len(files), files*3, [RTT,SRTT,MDEV]*len(files)) 
-		sc1 = create_script(fname + "_cwnd", ["cwnd", "cwnd"], files, [CWND]*len(files));
+if __name__ == '__main__':
 
-		scripts = [sc1, sc2]
+
+	if len(sys.argv) < 2:
+		print "No arguments given.\n" + help_str
+		sys.exit(1)
+
+	if sys.argv[1] == '--help': 
+		print help_str
+		sys.exit(0)
+
+	join = False
+	for arg in sys.argv[:-1]:
+		if arg == "--join":
+			join = True 
+
+	logname = sys.argv[-1]
+	fname = logname.split('.')[0]
+	files = log_reader(logname, join)
+
+
+	sc2 = create_script(fname + "_rtt", ["rtt","srtt","mdev"]*len(files), files*3, [RTT,SRTT,MDEV]*len(files)) 
+	sc1 = create_script(fname + "_cwnd", ["cwnd", "cwnd"], files, [CWND]*len(files));
+	scripts = [sc1, sc2]	
+
+
+
+
+	if join:
+
 		for script in scripts:
 			with open("{}.gp".format(fname), 'w+') as f:
 				f.write(script)
@@ -99,7 +140,9 @@ if __name__ == '__main__':
 			call(["gnuplot" , "<", "{}.gp".format(fname)])
 	else:
 		for file in files:	
-			for script in scripts:
+			sc1 = create_script(file + "_rtt", ["rtt", "srtt", "mdev"], [file]*3, [RTT,SRTT,MDEV])
+			sc2 = create_script(file + "_cwnd", ["cwnd"], [file], [CWND])
+			for script in [sc1, sc2]:
 				with open("{}.gp".format(file), 'w+') as f:
 					f.write(script.format(fname=file))
 
